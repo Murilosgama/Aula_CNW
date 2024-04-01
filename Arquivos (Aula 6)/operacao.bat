5bc20018e78908f4e02de75d9fa1496cec15bbd9
Murilosgama <murilosgama@gmail.com>
2024-04-01 12:39:06 -0300

    calculadora

--- Arquivos (Aula 6)/operacao.bat
+++ Arquivos (Aula 6)/operacao.bat
@@ -5,11 +5,12 @@ set title=%Operações%
 title %title%
 
 set /p num1=Digite o primeiro numero: 
-set /p num2=Digite o segundo numero:
+set /p num2=Digite o segundo numero: 
 echo.
 echo Escolha o operador:
 echo 1)Adicao 2)Subtracao 3)Multiplicacao 4)Divisao 5)Modulo
 choice /c "12345" /n /m "Escolha a operacao:"
+goto %errorlevel%
 
 :1 start
 set /a adi=%num1%+%num2%
